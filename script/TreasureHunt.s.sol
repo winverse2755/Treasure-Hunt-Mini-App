f1c1f7f389ffcf7060098e8499d4452044529368
WinVerse <winverse@WinVerses-MacBook-Pro.local>
2025-12-04 12:30:41 +0100

    Initial Commit

---
+++ script/TreasureHunt.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {TreasureHunt} from "../src/TreasureHunt.sol";
+
+contract TreasureHuntScript is Script {
+    TreasureHunt public counter;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        counter = new TreasureHunt();
+
+        vm.stopBroadcast();
+    }
+}
